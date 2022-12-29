ad5068d0b9a282d355adf15861cc5468469e2565
Andrei Savelyeu1 <andrei_savelyeu1@epam.com>
2022-12-29 10:25:26 +0300

    extracted helpers data

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,3 @@
-import React from "react";
 import "./App.css";
 import { Header } from "./header/Header";
 import { Main } from "./main/Main";
@@ -8,123 +7,12 @@ import { RemoteWork } from "./remote-work/RemoteWork";
 import { Contacts } from "./contacts/Contacts";
 import { Footer } from "./footer/Footer";
 import { IconContext } from "react-icons/lib";
-import socialNetwork from "./assets/images/socialNetwork.jpg";
-import todolist from "./assets/images/todolist.jpg";
-import { FaReact, FaInstagram } from "react-icons/fa";
 import {
-  AiOutlineFacebook,
-  AiOutlineGithub,
-  AiOutlineLinkedin,
-} from "react-icons/ai";
-import { HiOutlineUserCircle } from "react-icons/hi";
-import { IoLogoSass, IoIosCall } from "react-icons/io";
-import { RiUserLocationFill } from "react-icons/ri";
-import { SiCss3, SiHtml5, SiMinutemailer, SiRedux } from "react-icons/si";
-import { VscSettingsGear } from "react-icons/vsc";
-
-const skills = [
-  {
-    id: 1,
-    title: "React",
-    description:
-      "Lorem ipsum dolor sit amet, consectetur adipisicing elit, " +
-      "sed do eiusmod tempor incididunt ut labore et dolore magna aliqua Ut enim...",
-    icon: <FaReact />,
-  },
-  {
-    id: 2,
-    title: "Redux",
-    description:
-      "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod" +
-      " tempor incididunt ut labore et dolore magna aliqua Ut enim...",
-    icon: <SiRedux />,
-  },
-  {
-    id: 3,
-    title: "Rest API",
-    description:
-      "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do" +
-      " eiusmod" +
-      " tempor incididunt ut labore et dolore magna aliqua Ut enim...",
-    icon: <VscSettingsGear />,
-  },
-  {
-    id: 4,
-    title: "CSS",
-    description:
-      "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do" +
-      " eiusmod" +
-      " tempor incididunt ut labore et dolore magna aliqua Ut enim...",
-    icon: <SiCss3 />,
-  },
-  {
-    id: 5,
-    title: "HTML",
-    description:
-      "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do" +
-      " eiusmod" +
-      " tempor incididunt ut labore et dolore magna aliqua Ut enim...",
-    icon: <SiHtml5 />,
-  },
-  {
-    id: 7,
-    title: "Sass",
-    description:
-      "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do" +
-      " eiusmod" +
-      " tempor incididunt ut labore et dolore magna aliqua Ut enim...",
-    icon: <IoLogoSass />,
-  },
-];
-const projects = [
-  {
-    id: 1,
-    title: "Social Network",
-    description:
-      "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut" +
-      " labore et dolore magna aliqua Ut enim...",
-    projectBackground: {
-      backgroundImage: `url(${socialNetwork})`,
-    },
-  },
-  {
-    id: 2,
-    title: "Todolist",
-    description:
-      "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod" +
-      " tempor incididunt ut labore et dolore magna aliqua Ut enim...",
-    projectBackground: {
-      backgroundImage: `url(${todolist})`,
-    },
-  },
-];
-const socialLink = [
-  { id: 1, icon: <AiOutlineFacebook /> },
-  { id: 2, icon: <FaInstagram /> },
-  { id: 3, icon: <AiOutlineLinkedin /> },
-  { id: 4, icon: <AiOutlineGithub /> },
-];
-const contactInfo = [
-  {
-    id: 1,
-    icon: <HiOutlineUserCircle />,
-    title: "Name",
-    content: "Andrei Saveluey",
-  },
-  {
-    id: 2,
-    icon: <RiUserLocationFill />,
-    title: "Location",
-    content: "Belarus, Mogilev",
-  },
-  { id: 3, icon: <IoIosCall />, title: "Call Me", content: "+375292536635" },
-  {
-    id: 4,
-    icon: <SiMinutemailer />,
-    title: "Email Me",
-    content: "andreisaveluev@gmail.com",
-  },
-];
+  skills,
+  projects,
+  socialLinks,
+  contactInfos,
+} from "./assets/helpers/index";
 
 export type SkillType = {
   id: number;
@@ -159,10 +47,10 @@ const App = () => {
           <Skills skills={skills} />
           <Projects projects={projects} />
           <RemoteWork />
-          <Contacts contactInfo={contactInfo} />
+          <Contacts contactInfo={contactInfos} />
         </IconContext.Provider>
         <IconContext.Provider value={{ className: "footerIcons" }}>
-          <Footer socialLink={socialLink} />
+          <Footer socialLink={socialLinks} />
         </IconContext.Provider>
       </>
     </div>
---
+++ src/assets/helpers/index.js
@@ -0,0 +1,120 @@
+import { FaReact, FaInstagram } from "react-icons/fa";
+import {
+  AiOutlineFacebook,
+  AiOutlineGithub,
+  AiOutlineLinkedin,
+} from "react-icons/ai";
+import { HiOutlineUserCircle } from "react-icons/hi";
+import { IoLogoSass, IoIosCall } from "react-icons/io";
+import { RiUserLocationFill } from "react-icons/ri";
+import { SiCss3, SiHtml5, SiMinutemailer, SiRedux } from "react-icons/si";
+import { VscSettingsGear } from "react-icons/vsc";
+import socialNetwork from "../images/socialNetwork.jpg";
+import todolist from "../images/todolist.jpg";
+
+export const skills = [
+  {
+    id: 1,
+    title: "React",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipisicing elit, " +
+      "sed do eiusmod tempor incididunt ut labore et dolore magna aliqua Ut enim...",
+    icon: <FaReact />,
+  },
+  {
+    id: 2,
+    title: "Redux",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod" +
+      " tempor incididunt ut labore et dolore magna aliqua Ut enim...",
+    icon: <SiRedux />,
+  },
+  {
+    id: 3,
+    title: "Rest API",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do" +
+      " eiusmod" +
+      " tempor incididunt ut labore et dolore magna aliqua Ut enim...",
+    icon: <VscSettingsGear />,
+  },
+  {
+    id: 4,
+    title: "CSS",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do" +
+      " eiusmod" +
+      " tempor incididunt ut labore et dolore magna aliqua Ut enim...",
+    icon: <SiCss3 />,
+  },
+  {
+    id: 5,
+    title: "HTML",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do" +
+      " eiusmod" +
+      " tempor incididunt ut labore et dolore magna aliqua Ut enim...",
+    icon: <SiHtml5 />,
+  },
+  {
+    id: 7,
+    title: "Sass",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do" +
+      " eiusmod" +
+      " tempor incididunt ut labore et dolore magna aliqua Ut enim...",
+    icon: <IoLogoSass />,
+  },
+];
+
+export const projects = [
+  {
+    id: 1,
+    title: "Social Network",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut" +
+      " labore et dolore magna aliqua Ut enim...",
+    projectBackground: {
+      backgroundImage: `url(${socialNetwork})`,
+    },
+  },
+  {
+    id: 2,
+    title: "Todolist",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod" +
+      " tempor incididunt ut labore et dolore magna aliqua Ut enim...",
+    projectBackground: {
+      backgroundImage: `url(${todolist})`,
+    },
+  },
+];
+
+export const socialLinks = [
+  { id: 1, icon: <AiOutlineFacebook /> },
+  { id: 2, icon: <FaInstagram /> },
+  { id: 3, icon: <AiOutlineLinkedin /> },
+  { id: 4, icon: <AiOutlineGithub /> },
+];
+
+export const contactInfos = [
+  {
+    id: 1,
+    icon: <HiOutlineUserCircle />,
+    title: "Name",
+    content: "Andrei Saveluey",
+  },
+  {
+    id: 2,
+    icon: <RiUserLocationFill />,
+    title: "Location",
+    content: "Belarus, Mogilev",
+  },
+  { id: 3, icon: <IoIosCall />, title: "Call Me", content: "+375292536635" },
+  {
+    id: 4,
+    icon: <SiMinutemailer />,
+    title: "Email Me",
+    content: "andreisaveluev@gmail.com",
+  },
+];
